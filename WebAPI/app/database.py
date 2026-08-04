d596b9a1b00ee040edbfe0d64e9646f9036aadf0
Tristan <simonlalondetristan@hotmail.com>
2026-08-03 21:14:25 -0400

    Worked a bit on the Database and API to receive Products. Just as a test case.

--- WebAPI/app/database.py
+++ WebAPI/app/database.py
@@ -1,28 +1,35 @@
 import psycopg
 
 
+def get_connection():
+    return psycopg.connect(
+        host="database",
+        dbname="inventory_db",
+        user="temporaryadmin",
+        password="temporarypassword",
+        port=5432,
+    )
+
 def check_database():
-
     try:
-        connection = psycopg.connect(
-            host="database",
-            dbname="inventory_db",
-            user="temporaryadmin",
-            password="temporarypassword",
-            port=5432
-        )
-
-        cursor = connection.cursor()
-
-        cursor.execute("SELECT 1")
-
-        result = cursor.fetchone()
-
-        cursor.close()
-        connection.close()
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT 1")
+                result = cursor.fetchone()
 
         return result[0] == 1
 
     except Exception as e:
         print(e)
         return False
+
+def check_inventory():
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM products")
+                return cursor.fetchall()
+
+    except Exception as e:
+        print(e)
+        return []
--- WebAPI/app/main.py
+++ WebAPI/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from scripts.database import check_database
+from scripts.database import check_database, check_inventory
 
 
 app = FastAPI()
@@ -21,3 +21,22 @@ def health():
         "api": "healthy",
         "database": "healthy" if database_status else "unavailable"
     }
+
+@app.get("/products")
+def products():
+    inventory = check_inventory()
+
+    products = []
+
+    for row in inventory:
+        products.append({
+            "id": row[0],
+            "sku": row[1],
+            "name": row[2],
+            "stock": row[3],
+            "stock_critical": row[4],
+            "description": row[5],
+            "supplier_id": row[6]
+        })
+
+    return products
