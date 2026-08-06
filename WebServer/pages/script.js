42ea1f8ee69e641504e2846bb9cf5780ab974ede
Tristan <simonlalondetristan@hotmail.com>
2026-08-06 18:24:11 -0400

    Created Reverse Proxy and added HTML Landing page, Javascript to query the API and structure the JSON strings into formatted data. Can search for specific Item using the name, or display all Products. The page is VERY basic but will eventually get better.

---
+++ WebServer/pages/script.js
@@ -0,0 +1,58 @@
+const loadButton = document.getElementById("loadButton");
+loadButton.addEventListener("click", loadProducts);
+
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", () => {
+
+    const searchText = document.getElementById("productSearch").value;
+
+    searchProducts(searchText);
+
+});
+
+
+async function loadProducts(){
+
+	const response = await fetch("/api/products");
+
+	const products = await response.json();
+
+	const container = document.getElementById("products");
+
+	container.innerHTML = "";
+
+	products.forEach(product => {
+		container.innerHTML += `
+            <p>
+                ${product.id} |
+                ${product.name} |
+                Stock: ${product.stock}
+            </p>
+        `;
+
+    });
+
+}
+
+async function searchProducts(name){
+
+	const response = await fetch(`/api/products/search?name=${name}`);
+
+	const products = await response.json();
+
+	const container = document.getElementById("products");
+
+	container.innerHTML = "";
+
+	products.forEach(product => {
+		container.innerHTML += `
+            <p>
+                ${product.id} |
+                ${product.name} |
+                Stock: ${product.stock}
+            </p>
+        `;
+
+    });
+
+}
